b8a9a5a8f5311c1a531d04e0779c8ccd7e50f136
Jun Omae <jun66j5@gmail.com>
2010-03-10 19:39:06 +0900

    Fixed the problem that export report as xlshtml doesn't work. Added xlshtml template file for japanese locale.

--- tracmsofficexml/report.py
+++ tracmsofficexml/report.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
+import pkg_resources
 import re
 
 from trac.core import *
 from trac.mimeview import Context
 from trac.resource import Resource
+from trac.ticket.report import ReportModule
 from trac.util.text import unicode_urlencode
 from trac.web.api import IRequestFilter
 from trac.web.chrome import add_link
@@ -17,14 +20,6 @@ class ExcelReportModule(Component):
     implements(IRequestFilter)
 
     def pre_process_request(self, req, handler):
-        if re.match(r'/report/[0-9]+', req.path_info) \
-                and req.args.get('format') in ('xlshtml', 'excelxml') \
-                and handler.__class__.__name__ == 'ReportModule':
-            orig = handler.execute_paginated_report
-            def execute_report(req, db, id, sql, args, limit=0, offset=0):
-                del handler.execute_paginated_report
-                return orig.__call__(handler, req, db, id, sql, args, 1000000, 0)
-            handler.execute_paginated_report = execute_report
         return handler
 
     def post_process_request(self, req, template, data, content_type):
@@ -37,6 +32,9 @@ class ExcelReportModule(Component):
             else:
                 format = None
             if format is not None:
+                locale = _detect_locale()
+                if locale is not None:
+                    template = '%s.%s.html' % (template[:-5], locale)
                 content_type = 'application/vnd.ms-excel'
                 resource = Resource('report', req.args['id'])
                 data['context'] = Context.from_request(req, resource, absurls=True)
@@ -61,5 +59,36 @@ class ExcelReportModule(Component):
             href = '&' + unicode_urlencode(params)
         add_link(req, 'alternate', '?format=xlshtml' + href, 'Excel HTML', 'application/vnd.ms-excel', 'html')
         # add_link(req, 'alternate', '?format=excelxml' + href, 'Excel XML', 'application/vnd.ms-excel', 'xml')
+
+
+_page_names = {
+    'TracJa': 'ja',
+}
+
+def _detect_locale():
+    dir = pkg_resources.resource_filename('trac.wiki', 'default-pages') 
+    for name, locale in _page_names.iteritems():
+        if os.path.exists(os.path.join(dir, name)):
+            return locale
+    return None
+
+
+def _execute_paginated_report(self, req, db, id, sql, args, limit=0, offset=0):
+    excel = False
+    if re.match(r'/report/[0-9]+', req.path_info) \
+            and 'action' not in req.args \
+            and req.args.get('format') in ('xlshtml', 'excelxml') \
+            and self.env.is_component_enabled(ExcelReportModule):
+        excel = True
+        limit = 0
+        offset = 0
+    cols, results, num_items = _execute_paginated_report_orig(self, req, db, id, sql, args, limit, offset)
+    if excel:
+        num_items = len(results)
+    return cols, results, num_items
+
+
+_execute_paginated_report_orig = ReportModule.execute_paginated_report
+ReportModule.execute_paginated_report = _execute_paginated_report
 
 
